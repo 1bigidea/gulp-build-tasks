2e581022a78efe6d2a7c6151581adae807971bcb
Craig Simpson <c.simps@gmail.com>
2016-01-24 21:30:12 +0000

    Stop watching images, as when you drop one image into the directory it triggers all images to be processed.

--- tasks/watch.js
+++ tasks/watch.js
@@ -12,7 +12,6 @@ gulp.task( 'watch' , function() {
     // Set the watch tasks for different file type
     gulp.watch( config.styles , ['styles'] );
     gulp.watch( config.scripts , ['scripts'] );
-    gulp.watch( config.images , ['images'] );
     gulp.watch( config.code ).on( 'change' , browserSync.reload );
 
 });
